3627fe8b50f3539b457a4b0fb855eed1bfdb5a23
Robert Bevly <robertbevly@gmail.com>
2014-10-21 10:50:59 -0500

    small changes/bug fix

--- uniVote/accounts/views.py
+++ uniVote/accounts/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib.auth import authenticate, cationForm
+from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render_to_response, render
 from django.core.context_processors import csrf
 
@@ -50,6 +51,7 @@ def user_register(request):
             if form.is_valid:
                 form.save()
                 return render(request, 'accounts/registered.html')
+                #return HttpResponse('User created succcessfully.')
         else:
             form = UserRegisterForm()
         context = {}
