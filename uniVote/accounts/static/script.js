1482a18816f8656d7377ad9c1a860315e8f5403b
Kyle <ksibod@gmail.com>
2014-10-29 13:55:00 -0500

    Added test if user login credentials are incorrect. Shows popup if that happens.  Need to implement ajax somehow to make the page not refresh.  But it works for now.

--- uniVote/accounts/static/script.js
+++ uniVote/accounts/static/script.js
@@ -8,3 +8,17 @@ function dothefunc()
 	$("#login").slideToggle("slow");
 	console.log("yolo dawg");
 }
+
+
+// Function to show incorrect credentials popup on login page
+function invalidCredentials()
+{
+    console.log("INVALID!");
+    setTimeout(function() {
+        swal({   title: "Oops...",
+                 text: "Your username or password is invalid. Please try again.",
+                 type: "error"});
+    }, 500);
+    console.log("showing the popup");
+    return false;
+}
