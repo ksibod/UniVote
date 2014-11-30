b317e976cd6649695ddb13845995836d03c45780
Kyle Smith <ksibod@gmail.com>
2014-11-29 21:24:58 -0600

    Was making changes to the voting stuff.  About to merge it with what Bob did.

--- uniVote/accounts/static/homescript.js
+++ uniVote/accounts/static/homescript.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
 	dothefunc();
+
 });
 
 function dothefunc()
 {
 	$("#uniLogo").fadeIn("3000");
-}
+};
+
+    // when the user clicks on the register to vote button
+function registerToVote(election_id)
+{
+    console.log(election_id);
+
+    swal({
+      title: "Are you sure?",
+      text: "You will not be able to recover this imaginary file!",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel plx!",
+      closeOnConfirm: false,
+      closeOnCancel: false
+    },
+    function(isConfirm){
+      if (isConfirm) {
+        swal("Deleted!", "Your imaginary file has been deleted.", "success");
+      } else {
+            swal("Cancelled", "Your imaginary file is safe :)", "error");
+      }
+    });
+
+    return false;
+};
